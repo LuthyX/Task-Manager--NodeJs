3d5d571121d26fe36db6f461f90ca6cb773cd977
LuthyX <peteradegoke81@gmail.com>
2023-03-07 13:38:14 +0100

    feat : added more methods to my controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,64 @@
-const getAllTasks = (req, res)=>{
-    res.json({success: true, data:[]} )
+const Task = require('../models/task')
+
+
+const createTask = async (req, res)=>{
+    try {
+        const task = await Task.create(req.body)
+        return res.status(201).json({success: true, data:task})
+    }
+    catch(err){
+       return res.status(500).json({success: false, message: err})
+    }
+    
 }
-const getTask = (req, res)=>{
-    res.json({success :true, data:"OK"})
+const getAllTasks = async (req, res)=>{
+    try{
+        const tasks = await Task.find({});
+        res.status(200).json({success : true, data: tasks})
+    }
+    catch(err){
+        res.status(404).json({success:false, data:err})
+    }
 }
-const createTask = (req, res)=>{
-    console.log(req.body)
-    res.json({success :true, data:"OK"})
+const getTask = async(req, res)=>{
+    try{
+        const { id : taskId } = req.params
+        const task = await Task.findOne({_id: taskId });
+        console.log(task)
+        if( !task){
+            return res.status(404).json({success:false, message: `Task ID-${id} does not exist`});
+        }
+        return res.status(200).json({success:true, data : task})
+    }
+    catch(err){
+       return res.status(500).json({success:false, message :err})
+    }
 }
-const updateTask = (req, res)=>{
-    res.json({success :true, data:"OK"})
+const updateTask = async (req, res)=>{
+    try {
+        const { id : taskId} = req.params;
+        const task = await Task.findByIdAndUpdate({_id:taskId}, req.body, { new :true, runValidators:true})
+        if(!task){
+            return res.status(404).json({success:false, message: `Task ID-${id} does not exist`});
+        }
+        return res.status(200).json({success:true, data : task})
+    }
+    catch(err){
+        return res.status(500).json({success:false, message :err})
+    }
 }
-const deleteTask = (req, res)=>{
-    res.json({success :true, data:"OK"})
+const deleteTask = async (req, res)=>{
+   try{
+    const { id : taskId } = req.params
+    const task = await Task.findOneAndDelete({_id: taskId})
+    if (!task){
+        return res.status(404).json({success : false, message : `Task ID-${taskId} does not exist`})
+    }
+    return res.status(200).json({succes:true, data:task})
+   }
+   catch(err){
+    return res.status(500).json({success:false, message:err})
+   }
 }
 
 module.exports = {
