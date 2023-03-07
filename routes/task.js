6443807def934259be7d8f20994bcc9a860a4ae3
LuthyX <peteradegoke81@gmail.com>
2023-03-07 13:39:53 +0100

    feat : fixed some endpoints correctly

--- routes/task.js
+++ routes/task.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { getTask, getAllTasks, createTask, deleteTask, updateTask } = require('../controllers/tasks')
 
-router.get('/', getTask);
-router.get('/:id', getAllTasks)
+router.get('/:id', getTask);
+router.get('/', getAllTasks)
 router.post('/', createTask)
-router.patch(':id', updateTask)
-router.delete('/:id')
+router.patch('/:id', updateTask)
+router.delete('/:id', deleteTask)
+
 module.exports = router
