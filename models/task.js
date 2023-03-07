044a5e9b5ee1993fd37ef9c17d0c2a8abda95672
LuthyX <peteradegoke81@gmail.com>
2023-03-07 13:39:16 +0100

    feat: added more structure to my schema

--- models/task.js
+++ models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    isCompleted : Boolean,
+    name: {
+        type: String,
+        required: [true, 'Name cannot be void'],
+        trim : true,
+        maxlength : [20, 'Name can not exceed 20 character limit']
+    },
+    isCompleted : {
+        type: Boolean,
+        default : false
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
